7bc6aa5c9aebc8915939f103483175049ba675b1
ceconley4 <ceconley4@gmail.com>
2018-11-27 14:51:30 -0500

    Finished auth
    Auth is now working 100%, with messages and the fields are being hidden
    at the appropriate times.

--- assets/scripts/auth/authui.js
+++ assets/scripts/auth/authui.js
@@ -4,19 +4,19 @@ $('#change-password').hide()
 $('#sign-out').hide()
 
 const signUpSuccess = function () {
-  $('#sign-in-success').html('Signed Up Successfully')
+  $('#authMessage').html('Signed Up Successfully')
   $('#sign-up')[0].reset()
   $('#sign-in')[0].reset()
 }
 
 const signUpFailure = function () {
-  $('#sign-in-success').html('Signed Up Failed')
+  $('#authMessage').html('Signed Up Failed')
   $('#sign-up')[0].reset()
 }
 
 const signInSuccess = function (data) {
   store.user = data.user
-  $('#sign-in-success').html('Signed In Successfully')
+  $('#authMessage').html('Signed In Successfully')
   $('#sign-in')[0].reset()
   $('#sign-up')[0].reset()
   $('#sign-in').hide()
@@ -26,22 +26,22 @@ const signInSuccess = function (data) {
 }
 
 const signInFailure = function () {
-  $('#sign-in-success').html('Signed In Failed')
+  $('#authMessage').html('Signed In Failed')
   $('#sign-in')[0].reset()
 }
 
 const changePasswordSuccess = function () {
-  $('#change-password-success').html('Password changed successfully')
+  $('#authMessage').html('Password changed successfully')
   $('#change-password')[0].reset()
 }
 
 const changePasswordFailure = function () {
-  $('#change-password-success').html('Error on password change')
+  $('#authMessage').html('Error on password change')
   $('#change-password')[0].reset()
 }
 
 const signOutSuccess = function () {
-  $('#sign-out-success').html('Signed Out Successfully')
+  $('#authMessage').html('Signed Out Successfully')
   store.user = null
   $('#sign-in').show()
   $('#sign-up').show()
@@ -50,7 +50,7 @@ const signOutSuccess = function () {
 }
 
 const signOutFailure = function () {
-  $('#sign-out-success').html('Signed Out Failed')
+  $('#authMessage').html('Signed Out Failed')
 }
 
 module.exports = {
