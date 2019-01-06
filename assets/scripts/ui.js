d43112db470c9db567c426370e0baa582f44737b
ceconley <ceconley4@gmail.com>
2019-01-05 19:44:09 -0500

    handlebars for showing single item
    I was able to show a single item using handle bars.
    Now the table is being rendered by handlebars.

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -16,14 +16,6 @@ const onShowSuccess = (response) => {
   const showExpenseHtml = showOneExpense({ expense: response })
   $('#results').empty()
   $('#results').html(showExpenseHtml)
-  // const expenseHTML = (`
-  //   <h6>ID: ${response.expense.id}</h6>
-  //   <p>Source: ${response.expense.source}</p>
-  //   <p>Amount: ${response.expense.amount}</p>
-  //   <p>Category: ${response.expense.category}</p>
-  //   <p>Description: ${response.expense.description}</p>
-  //   `)
-  // $('#results').html(expenseHTML)
 }
 
 const onShowFailure = () => {
@@ -36,16 +28,6 @@ const onIndexSuccess = (response) => {
   const showExpensesHtml = showAllExpenses({ expenses: response.expenses })
   $('#results').empty()
   $('#results').append(showExpensesHtml)
-  // response.expenses.forEach(expense => {
-  //   const expenseHTML = (`
-  //     <h6>ID: ${expense.id}</h6>
-  //     <p>Source: ${expense.source}</p>
-  //     <p>Amount: ${expense.amount}</p>
-  //     <p>Category: ${expense.category}</p>
-  //     <p>Description: ${expense.description}</p>
-  //     `)
-  //   $('#results').append(expenseHTML)
-  // })
 }
 
 const onIndexFailure = () => {
